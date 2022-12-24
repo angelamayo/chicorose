32ac12ea8947257a25e35c960eca557a5a2c0dd2
angelamayo <52805277+angelamayo@users.noreply.github.com>
2022-12-24 16:07:35 +0100

    Update PlaceOrderScreen.js

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -97,7 +97,7 @@ export default function PlaceOrderScreen() {
                 <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
                 <strong>Mobile:</strong> {cart.shippingAddress.mobile} <br />
                 <strong>Address: </strong> {cart.shippingAddress.address},
-                {cart.shippingAddress.city}, {cart.shippingAddress.postalCode},
+                {cart.shippingAddress.city}, {cart.shippingAddress.postalCode}
                 {cart.shippingAddress.country}
               </Card.Text>
               <Link to="/shipping">Edit</Link>
