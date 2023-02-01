27981bd55b3b66819b833a48cc7350336e913a6c
angelamayo <52805277+angelamayo@users.noreply.github.com>
2023-02-01 02:11:20 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useContext, useEffect, useState } from 'react';
 import { Store } from './Store';
